063edecfb661ce894c39071f3a7fc1deebd620dd
shwetasatpute709-eng <shwetasatpute709@gmail.com>
2025-10-10 22:03:44 +0530

    Update addition1new.c

--- c programming/assignments/assignment 0/addition1.c
+++ c programming/assignments/assignment 0/addition1.c
@@ -1,6 +1,19 @@
 void main()
 {
-	int r=5,area;
-	area=3.14*r*r; 
-	printf("area of circle is %d",area);
+	int a=10,b=20,c;
+	c=a+b;
+	printf("addition of %d and %d is %d \n",a,b,c);
+	c=a-b;
+		printf("substraction of %d and %d is %d \n",a,b,c);
+	c=a*b;
+		printf("multiplication of %d and %d is %d \n",a,b,c);
+	c=a/b;
+		printf("division of %d and %d is %d \n",a,b,c);
+	c=a%b;
+		printf("module of %d and %d is %d \n",a,b,c);
+
+
+
+
+
 }
